7626ec2c7482f818e8301470e7d7d1ab301e7d5f
write-code-b <minjichoi.official@gmail.com>
2024-11-16 16:47:26 +0900

    footer 구현

---
+++ react-board/src/pages/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <>
+      <div id="footer">
+        <div>
+          <a target="_blank" href="https://github.com/write-code-b/Conan-Card">
+            © 2024 website by @write_code_b
+          </a>
+          <a target="_blank" href="https://icons8.com">
+            icon by Icons8
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- react-board/src/pages/Home.jsx
+++ react-board/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import MainSection from "./MainSection";
+import Footer from "./Footer";
 
 function Home() {
   return (
     <>
       <Header />
       <MainSection />
+      <Footer/>
     </>
   );
 }
