88ee285435126e5844c5d8ca380ee68de6539d52
write-code-b <minjichoi.official@gmail.com>
2024-11-17 21:50:11 +0900

    메인화면의 사용자 정보 부분 수정

--- react-board/src/pages/MainSection.jsx
+++ react-board/src/pages/MainSection.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../provider/authProvider";
+import { jwtDecode } from "jwt-decode";
 
 function MainSection() {
   const { token } = useAuth();
+  const decodedToken = jwtDecode(token);
 
   return (
     <>
@@ -11,8 +13,8 @@ function MainSection() {
           <div id="userInfo">
             {token && <img src="../public/profile.png" className="profile" alt="" />}
             <div>
-              <span className="name">안녕하세요,</span>
-              {token && <span className="userId">아이디 ({localStorage.getItem("userId")})</span>}
+              {token && <span className="name">안녕하세요, {decodedToken.name}님.</span>}
+              {token && <span className="userId">아이디 ({decodedToken.username})</span>}
               {!token && (
                 <span>
                   <Link to="/login">
